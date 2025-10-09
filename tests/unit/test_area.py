227c365d7da13b3e5514610ae757d0380ed9fb2c
atmisboru <boruatmis@163.com>
2025-10-09 20:42:38 +0800

    Update test_area.py

--- tests/unit/test_area.py
+++ tests/unit/test_area.py
@@ -3,6 +3,7 @@ from src.area import calculate_area_square
 
 def test_calculate_area_square():
     assert calculate_area_square(2) == 4
+    //test 
     assert calculate_area_square(2.5) == 6.25
 
 def test_calculate_area_square_negative():
